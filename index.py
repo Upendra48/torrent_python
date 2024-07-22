a63aaf19539bde72be1f0e047e6a5b2f84f0c2c5
Upendra Raj Joshi <upendraofficial10@gmail.com>
2024-07-22 23:39:53 +0545

    Add announce request and response

---
+++ index.py
@@ -0,0 +1,15 @@
+import torrent_parser
+import tracker
+
+def main():
+    # Open and decode the torrent file
+    torrent = torrent_parser.open('catpicture_0002_archive.torrent')
+
+    # Get peers from the tracker
+    def print_peers(peers):
+        print('List of peers:', peers)
+    
+    tracker.get_peers(torrent, print_peers)
+
+if __name__ == '__main__':
+    main()
